24088a3ca7ff48d7c4a3f6f9158337800d145f24
Valentina <gassetvale141@gmail.com>
2026-07-07 13:48:20 -0400

    creacion de la opcion agregar

--- sistema_de_productos.py
+++ sistema_de_productos.py
@@ -20,4 +20,54 @@ def mostrar_opt():
             print("Error: Ingrese un dato valido (numero entero)")
     return opcion
 
+def mostrar_nombre_producto():
+    validar_nombre = False
+    while not validar_nombre:
+        nombre_producto = input("Ingrese nombre: ").strip().lower()
+        if nombre_producto == "":
+            print("Error, nombre no puede ir vacio")
+        else:
+            validar_nombre = True
+            break 
+    return nombre_producto
+
+nombre = mostrar_nombre_producto()
 opt = mostrar_opt
+
+def mostrar_stock():
+    validar_stock = False
+    while not validar_stock:
+        try:
+            cantidad_stock = int(input("Ingrese la cantidad de stock: "))
+            if cantidad_stock >= 0 and cantidad_stock <= 500:
+                validar_stock = True
+                break 
+            else:
+                print("Error, ingrese una cantidad entre 0 y 500")
+        except ValueError:
+            print("Error, ingrese un valor valido (numero entero)")
+    return cantidad_stock
+
+stock = mostrar_stock()
+
+def mostrar_precio():
+    validar_precio = False
+    while not validar_precio:
+        try:
+            precio_producto = float(input("Ingrese el precio del producto: "))
+            if precio_producto > 0 :
+                validar_precio = True
+                break 
+            else:
+                print("Error: ingrese un numero mayor que 0")
+        except ValueError:
+            print("Error: Ingrese un numero decimal")
+    return precio_producto
+
+precio = mostrar_precio()
+
+
+
+productos = []
+productos_disponibles = 500
+
